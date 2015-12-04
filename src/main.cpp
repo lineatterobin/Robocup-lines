d44f4441dce44b1f8070784c71468854fbeb8647
Rlineatte <lineatterobin@gmail.com>
2015-12-04 12:12:10 +0100

    Expérimentations

--- src/main.cpp
+++ src/main.cpp
@@ -33,15 +33,16 @@ void process(char* imsname, char* imfield)
     Mat imf = imread(imfield, CV_LOAD_IMAGE_GRAYSCALE);
     Mat imsG;
 
+    imshow("ims",ims);
+
     // Brightness & contrast
     for( int y = 0; y < ims.rows; y++ )
         for( int x = 0; x < ims.cols; x++ )
             for( int c = 0; c < 3; c++ )
                 new_ims.at<Vec3b>(y,x)[c] = saturate_cast<uchar>( 2 * ims.at<Vec3b>(y,x)[c] );
 
-    namedWindow("Source");
-    imshow( "Source", new_ims );
-    waitKey(0);
+    imshow( "ims (contrast)", new_ims );
+    //waitKey(0);
 
 
     //Convert main image to a gray scale
@@ -65,44 +66,48 @@ void process(char* imsname, char* imfield)
                 hsv[0].at<uchar>(y,x) =  (abs((int)hsv[0].at<uchar>(y,x) - 66) < 20) * 255;
 */
 
-    namedWindow("Source");
-    imshow( "Source", hsv[2]);
-    waitKey(0);
+    imshow( "HSV Value1", hsv[2]);
+    //waitKey(0);
 
+    //bilateralFilter(hsv[2].clone(), hsv[2], -1, 30, 10);
     threshold(hsv[2], hsv[2], 225, 255, THRESH_BINARY);
 
-    namedWindow("Source");
-    imshow( "Source", hsv[2]);
-    waitKey(0);
+    imshow( "HSV Value", hsv[2]);
+    //waitKey(0);
 
     //////////////////////////////////////////////////////////////////////////////////////
     //equalizeHist(imsG,imsG);
     bilateralFilter(imsG.clone(), imsG, -1, 30, 10);
-
-    namedWindow("Source");
-    imshow( "Source", imsG );
-    waitKey(0);
+    imshow( "1.1 Filter", imsG );
+    morphologyEx(imsG,imsG,CV_MOP_DILATE, getStructuringElement(MORPH_ELLIPSE, Size(8,8)));
+    /*for( int y = 0; y < imsG.rows; y++ )
+        for( int x = 0; x < imsG.cols; x++ )
+                imsG.at<uchar>(y,x) = saturate_cast<uchar>( 2.5 * imsG.at<uchar>(y,x));*/
+    equalizeHist(imsG,imsG);
+    imshow( "1.15 Filter", imsG );
+    //waitKey(0);
 
     adaptiveThreshold(imsG, imsG, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 3, 0);
 
-    namedWindow("Source");
-    imshow( "Source", imsG );
-    waitKey(0);
+    //imshow( "1.2 Threshold", imsG );
+    //waitKey(0);
 
     bilateralFilter(imsG.clone(), imsG, -1, 80, 10);
+
     adaptiveThreshold(imsG, imsG, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 3, 0);
     //threshold(imsG,imsG,125,255,CV_THRESH_BINARY);
     Mat kern = getStructuringElement(MORPH_CROSS, Size(8,8));
     morphologyEx(imsG,imsG,CV_MOP_CLOSE, kern);
     //morphologyEx(imsG,imsG,CV_MOP_ERODE, getStructuringElement(MORPH_CROSS, Size(3,3)));
 
-    focus(imsG, imf);
-    namedWindow("Source");
-    imshow( "Source", imsG );
-    imshow("ims",ims);
+    //focus(imsG, imf);
+
+    imshow( "1.3 Morpho", imsG );
+
     // Process and display execution time
     time = ((double)getTickCount() - time) / getTickFrequency();
     cout << "Execution time:" << time << endl;
+
     waitKey(0);
 
 }
